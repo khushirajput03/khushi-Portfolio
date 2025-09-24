283e56a924b34e0b06c6672bbacfeb55a1d82788
khushi <khushikhushi89978@gmail.com>
2025-09-25 01:08:37 +0530

    Fixed ESLint errors and updated globe, infinite cards, and sticky scroll components

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -6,31 +6,35 @@ import { IoIosSend } from "react-icons/io";
 
 const HeroSection = () => {
   return (
-    <div className='h-auto md:h-[40rem] w-full  flex flex-col items-center justify-center relative overflow-hidden py-10 md:py-0 w-full min-h-screen'>
-        <Spotlight
+    <div className='h-auto md:h-[40rem] w-full flex flex-col items-center justify-center relative overflow-hidden py-10 md:py-0 min-h-screen'>
+      <Spotlight
         className="-top-40 md:-top-20 md:left-60 z-10 w-full"
         fill="white"
       />
 
-        <div className='p-4 relative z-10 w-full text-center'>
-            
-            <h1 className='mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-40'>Building complete web apps from</h1>
-             <h1 className='mt-70 md:mt-0 text-4xl  md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-40'>frontend to backend.</h1>
-            <h1 className='mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-40'></h1>
-            <p className='mt-8 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>Hi, I'm Khushi — a Full Stack Developer
-            </p>
-            <div className='mt-10'>
-                <Link href={"#recent-project"}>
-                 <Button
-        borderRadius="1.75rem"
-        className="p-4  dark:bg-slate-900 gap-2 text-black dark:text-white border-neutral-200 dark:border-slate-800"
-      >
-        Show my Work <IoIosSend />
-      </Button>
+      <div className='p-4 relative z-10 w-full text-center'>
+        <h1 className='mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-40'>
+          Building complete web apps from
+        </h1>
+        <h1 className='mt-6 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-40'>
+          frontend to backend.
+        </h1>
 
-                </Link>
-            </div>
+        <p className='mt-8 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>
+          Hi, I&apos;m Khushi — a Full Stack Developer
+        </p>
+
+        <div className='mt-10'>
+          <Link href={"#recent-project"}>
+            <Button
+              borderRadius="1.75rem"
+              className="p-4 dark:bg-slate-900 gap-2 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Show my Work <IoIosSend />
+            </Button>
+          </Link>
         </div>
+      </div>
     </div>
   );
 };
