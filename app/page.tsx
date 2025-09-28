5dabd0ac96a1a3998dc6bf5bcb53a307d2db4457
khushi <khushikhushi89978@gmail.com>
2025-09-28 16:27:42 +0530

    nav.jsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       
       <HeroSection/>
-       {/* <Grid/> */}
+       <Grid/>
       <RecentProjects/>
       {/* <Clients/> */}
       <Experience/>
