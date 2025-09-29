d88a2ccfa0ae45077b59c7ecb166770200f1adc3
khushi <khushikhushi89978@gmail.com>
2025-09-29 15:46:01 +0530

    res

--- data/index.ts
+++ data/index.ts
@@ -10,8 +10,8 @@ export const gridItems = [
     id: 1,
     title: "I prioritize client collaboration, fostering open communication",
     description: "",
-    className: `lg:ml-0 lg:col-span-3 md:col-span-6 lg:row-span-4 md:row-span-1 lg:h-[110vh] lg:w-[55vw]  md:w-[120vw] lg:ml-0
-     w-[88vw] h-[40vh]`,
+    className: `lg:ml-0 lg:col-span-3 md:col-span-6 lg:row-span-4 md:row-span-1 lg:h-[110vh] lg:w-[55vw]  md:w-[120vw]
+     w-[88vw] h-[40vh] md:mr-10`,
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
     img: "/b1.svg",
@@ -21,7 +21,7 @@ export const gridItems = [
     id: 2,
     title: "I'm very flexible with time zone communications",
     description: "",
-    className: `lg:ml-0 lg:col-span-2  md:col-span-3 md:row-span-1 lg:row-span-2 md:w-[120vw] w-[88vw] h-[40vh] lg:ml-0 lg:w-[35vw] lg:h-[55vh]`,
+    className: `lg:ml-0 lg:col-span-2  md:col-span-3 md:row-span-1 lg:row-span-2 md:w-[120vw] w-[88vw] h-[40vh] lg:ml-0 lg:w-[35vw] lg:h-[55vh] md:mr-10`,
     imgClassName: "",
     titleClassName: "justify-start",
     img: "",
@@ -32,7 +32,7 @@ export const gridItems = [
     title: "My tech stack",
     description: "I constantly try to improve",
     className: `lg:ml-0 lg:col-span-2 md:col-span-3 md:row-span-1 lg:row-span-2 lg:w-[35vw] lg:ml-0 lg:h-[50vh]
-    md:col-span-6 md:w-[120vw]  w-full h-auto min-h-fit w-[88vw] h-[40vh] 
+    md:col-span-6 md:w-[120vw]  w-full h-auto min-h-fit w-[88vw] h-[40vh] md:mr-10
     `,
     imgClassName: "",
     titleClassName: "justify-center",
@@ -44,7 +44,7 @@ export const gridItems = [
     title: "Tech enthusiast with a passion for development.",
     description: "",
     className: `lg:ml-0 lg:col-span-2 md:col-span-3  lg:ml-0 lg:w-[35vw] lg:h-[25vh]
-    md:w-[55vw] md:h-[38vh] w-[88vw] h-[20vh]
+    md:w-[55vw] md:h-[38vh] w-[88vw] h-[20vh] md:mr-10
     `,
     imgClassName: "",
     titleClassName: "justify-start",
@@ -56,7 +56,7 @@ export const gridItems = [
     id: 5,
     title: "Currently building a JS Animation library",
     description: "The Inside Scoop",
-    className: `lg:ml-0 md:col-span-3 md:row-span-2 lg:w-[55vw] md:w-[60vw] md:h-[63vh] lg:h-[69vh] w-[88vw] h-[20vh]`,
+    className: `lg:ml-0 md:col-span-3 md:row-span-2 lg:w-[55vw] md:w-[60vw] md:h-[63vh] lg:h-[69vh] w-[88vw] h-[20vh] md:mr-10`,
     imgClassName: `absolute right-0 bottom-0 w-60 `,
     titleClassName: "justify-center md:justify-start lg:justify-center",
     img: "/b5.svg",
@@ -66,7 +66,7 @@ export const gridItems = [
     id: 6,
     title: "Do you want to start a project together?",
     description: "",
-    className: `lg:ml-0 lg:col-span-2 md:col-span-3 lg:ml-0 lg:h-[40vh] lg:w-[35vw]
+    className: `lg:ml-0 lg:col-span-2 md:col-span-3 lg:ml-0 lg:h-[40vh] lg:w-[35vw] md:mr-10
     md:w-[55vw] md:h-[20vh]5 w-[88vw] `,
     imgClassName: "",
     titleClassName: "justify-center md:max-w-full max-w-60 text-center",
