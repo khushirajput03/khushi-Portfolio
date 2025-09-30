046f00fd83633c1576077a85f46201d0f547c241
khushi <khushikhushi89978@gmail.com>
2025-09-30 12:06:30 +0530

    fix issue

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       
       <HeroSection/>
-       <Grid/>
+       {/* <Grid/> */}
       <RecentProjects/>
       <SparklesPreview/>
         <SkillsWaterfallStack/>  
