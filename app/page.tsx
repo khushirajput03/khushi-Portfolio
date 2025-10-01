8cfa48a760cb4320066a17d3c4d5c5e759241201
khushi <khushikhushi89978@gmail.com>
2025-10-01 11:24:47 +0530

    fix issue

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
       <HeroSection/>
        <Grid/>
       <RecentProjects/>
-      {/* <SparklesPreview/>
-      <SkillsWaterfallStack/>  
+       <SparklesPreview/>
+      {/*<SkillsWaterfallStack/>  
       <Experience/>
       <CanvasRevealEffectDemo/>
       <Footer/> */}
